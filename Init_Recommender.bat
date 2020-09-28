a8ad00d1a84528453b95482d72b323b82b506457
Dominik Heinz <dominik.heinz@ibm.com>
2020-09-28 12:30:00 +0200

    Added two exemplary files

---
+++ Init_Recommender.bat
@@ -0,0 +1,14 @@
+python project_init.py -n RecommenderEngine ^
+ -u "Dominik Heinz" ^
+ -m "dheinz0989@gmail.com" ^
+ -rd y ^
+ -v 3.8 ^
+ -f app.py ^
+ -r C:\Daten\Trainings\Recommender ^
+ -e rec ^
+ -eng anaconda ^
+ -j lab ^
+ -g n ^
+ -p numpy pandas scikit-learn ^
+ -owr y
+ 
